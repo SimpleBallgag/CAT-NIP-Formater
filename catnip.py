01f0b068eb7d529e4f98de931ce21116c52770be
SimpleBallgag <77182329+SimpleBallgag@users.noreply.github.com>
2021-01-08 22:43:58 -0500

    Create catnip.py

---
+++ catnip.py
@@ -0,0 +1,37 @@
+import random
+import PySimpleGUI as sg
+sg.SetOptions(text_element_background_color='#6D7993', button_color=("#030314", "#6D7993"), background_color="#9099A2",font=("Helvetica", 20,))
+
+
+catnip_column = [[sg.Text('Entity Name:'), sg.InputText(size = (20, 1), do_not_clear=True, key = "_NAME_"),
+
+sg.Text("Entity Attribute"), sg.InputText(size = (20, 1), do_not_clear=True, key = "_ATTRIBUTE_")],
+[sg.Text("Attribute Trait"), sg.InputText(size = (20, 1), do_not_clear=True, key = "_TRAIT_"),
+sg.Text("Trait Tags (seperate with a '/')"), sg.InputText(size = (20, 1), do_not_clear=True, key = "_TAGS_")],
+                 [sg.Multiline("CAT<NIP> to appear here", size=(80,5), key = "_ROLLSOUTPUT_", do_not_clear=True)],
+                 [sg.Button("CATNIP!"), sg.Text("Let's go!")]]
+
+layout = [[sg.Column(catnip_column, background_color="#6D7993")]]
+  
+window = sg.Window('CAT<NIP> Generator').Layout(layout)
+
+
+
+
+while True: 
+    event, values = window.Read()
+#    print(event)
+#    print(values)
+    output = ""
+    if event is None or event == 'Exit':
+        break
+    if event == "CATNIP!" and values["_NAME_"] and values["_ATTRIBUTE_"]:
+        try:
+            output = f'[NAME<{values["_NAME_"]}>:{values["_NAME_"]}<first>;\n'
+            output = output+f'{values["_ATTRIBUTE_"]}<{values["_NAME_"]}>:{values["_TRAIT_"]}<{values["_TAGS_"]}>.]'
+            window.FindElement('_ROLLSOUTPUT_').Update(output)
+        except:
+            pass
+      
+
+window.Close()
