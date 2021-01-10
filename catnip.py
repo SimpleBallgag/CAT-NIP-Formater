087f023c8f7f74a1a1f34dec87156eb5d0b7881e
SimpleBallgag <77182329+SimpleBallgag@users.noreply.github.com>
2021-01-10 00:36:09 -0500

    Update catnip.py

--- catnip.py
+++ catnip.py
@@ -8,7 +8,7 @@ catnip_column = [[sg.Text('Entity Name:'), sg.InputText(size = (20, 1), do_not_c
 sg.Text("Entity Attribute"), sg.InputText(size = (20, 1), do_not_clear=True, key = "_ATTRIBUTE_")],
 [sg.Text("Attribute Trait"), sg.InputText(size = (20, 1), do_not_clear=True, key = "_TRAIT_"),
 sg.Text("Trait Tags (seperate with a '/')"), sg.InputText(size = (20, 1), do_not_clear=True, key = "_TAGS_")],
-                 [sg.Multiline("CAT<NIP> to appear here", size=(80,5), key = "_ROLLSOUTPUT_", do_not_clear=True)],
+                 [sg.Multiline("CAT<NIP> to appear here", size=(80,5), key = "_OUTPUT_", do_not_clear=True)],
                  [sg.Button("CATNIP!"), sg.Text("Let's go!")]]
 
 layout = [[sg.Column(catnip_column, background_color="#6D7993")]]
@@ -16,20 +16,23 @@ layout = [[sg.Column(catnip_column, background_color="#6D7993")]]
 window = sg.Window('CAT<NIP> Generator').Layout(layout)
 
 
-
-
+first=1
+output = "["
 while True: 
     event, values = window.Read()
 #    print(event)
 #    print(values)
-    output = ""
     if event is None or event == 'Exit':
         break
     if event == "CATNIP!" and values["_NAME_"] and values["_ATTRIBUTE_"]:
         try:
-            output = f'[NAME<{values["_NAME_"]}>:{values["_NAME_"]}<first>;\n'
+            if first:
+                output = output+f'NAME<{values["_NAME_"]}>:{values["_NAME_"]}<first>;\n'
+                first=0
             output = output+f'{values["_ATTRIBUTE_"]}<{values["_NAME_"]}>:{values["_TRAIT_"]}<{values["_TAGS_"]}>.]'
-            window.FindElement('_ROLLSOUTPUT_').Update(output)
+            window.FindElement('_OUTPUT_').Update(output)
+            output=output[:-2]
+            output = output+';\n'
         except:
             pass
       
