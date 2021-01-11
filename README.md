2257fcb296135e0418b863cc075a08cb14e5fea9
SimpleBallgag <77182329+SimpleBallgag@users.noreply.github.com>
2021-01-10 22:34:46 -0500

    Updates
    Dropdown and setting edits

--- README.md
+++ README.md
@@ -5,7 +5,17 @@ This is a CATNIP formating tool for AI Dungeon. This formating method was create
 ## WIP
 This code is a work in progress
 
+## How to use it
+Fill in the blanks with the information you want to convert into the CATNIP format, example below
+![cumpire](img/cumpire.png)
+
+
 ## Features to add
-Expand the potential inputs
-Add catches for bad data
-POLISH IT UP
+
+### Expand the potential inputs
+Currently the formater only supports a limited number of inputs. Only one TRAIT can be supported per line, but traits can support multiple tags. 
+### Add catches for bad data
+
+### POLISH IT UP
+
+### Export to apk
--- catnip.py
+++ catnip.py
@@ -1,15 +1,44 @@
 import random
 import PySimpleGUI as sg
+attributes={
+    "Appearance" : "APPE", 
+    "Traits" : "TRAI", 
+    "Wearing" : "WEAR", 
+    "Gear" : "GEAR", 
+    "Speaking" : "SPEA", 
+    "Mentality" : "MENT", 
+    "Occupation" : "OCCU", 
+    "Skills" : "SKIL", 
+    "Relationships" : "RELA", 
+    "Situation" : "SITU", 
+    "Motivations" : "MOTI"
+    }
+character = {
+    "NAME" : "",
+    "APPE" : "",
+    "TRAI" : "",
+    "WEAR" : "",
+    "GEAR" : "",
+    "SPEA" : "",
+    "MENT" : "",
+    "OCCU" : "",
+    "SKIL" : "",
+    "RELA" : "",
+    "SITU" : "",
+    "MOTI" : ""
+    }
+
+choices=list(attributes.keys())
 sg.SetOptions(text_element_background_color='#6D7993', button_color=("#030314", "#6D7993"), background_color="#9099A2",font=("Helvetica", 20,))
 
 
 catnip_column = [[sg.Text('Entity Name:'), sg.InputText(size = (20, 1), do_not_clear=True, key = "_NAME_"),
 
-sg.Text("Entity Attribute"), sg.InputText(size = (20, 1), do_not_clear=True, key = "_ATTRIBUTE_")],
-[sg.Text("Attribute Trait"), sg.InputText(size = (20, 1), do_not_clear=True, key = "_TRAIT_"),
+sg.Text("Entity Attribute"), sg.InputCombo(choices,size = (20, 1), key = "_ATTRIBUTE_")],
+[sg.Text("Attribute Trait (seperate with a '/')"), sg.InputText(size = (20, 1), do_not_clear=True, key = "_TRAIT_"),
 sg.Text("Trait Tags (seperate with a '/')"), sg.InputText(size = (20, 1), do_not_clear=True, key = "_TAGS_")],
                  [sg.Multiline("CAT<NIP> to appear here", size=(80,5), key = "_OUTPUT_", do_not_clear=True)],
-                 [sg.Button("CATNIP!"), sg.Text("Let's go!")]]
+                 [sg.Button("CATNIP!"),sg.Button("RESET"),sg.Button("SAVE EDIT")]]
 
 layout = [[sg.Column(catnip_column, background_color="#6D7993")]]
   
@@ -29,12 +58,22 @@ while True:
             if first:
                 output = output+f'NAME<{values["_NAME_"]}>:{values["_NAME_"]}<first>;\n'
                 first=0
-            output = output+f'{values["_ATTRIBUTE_"]}<{values["_NAME_"]}>:{values["_TRAIT_"]}<{values["_TAGS_"]}>.]'
+            output = output+f'{attributes[values["_ATTRIBUTE_"]]}<{values["_NAME_"]}>:{values["_TRAIT_"]}<{values["_TAGS_"]}>.]'
+            output = output.replace( '<>','')
             window.FindElement('_OUTPUT_').Update(output)
             output=output[:-2]
             output = output+';\n'
         except:
             pass
+    if event == "RESET" :
+        output = ""
+        window.FindElement('_OUTPUT_').Update(output)
+        output = "["
+    if event == "SAVE EDIT" :
+        output = values["_OUTPUT_"]
+        window.FindElement('_OUTPUT_').Update(output)
+        output=output[:-3]
+        output = output+';\n'
       
 
 window.Close()
